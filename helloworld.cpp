b39edd5c9c27d6cc87ea478e23e1224a3236e36f
Ishara Harshana <isharaharshana06@gmail.com>
2025-02-07 16:30:30 +0530

    updated

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string name = "Alex";
+  int num1 = 10;
+  cout << "My name is " << name << "\n";
+  cout << "I'm " << num1 << " years old.";
+  return 0;
+}
