f8aa6e2fcdea3815ee4e78032343a7705dcfc760
Ishara Harshana <isharaharshana06@gmail.com>
2025-02-07 16:50:22 +0530

    updated

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+
+    cout << "Enter you name: " << endl;
+    cin >> name;
+    cout << "Enter age: " << endl;
+    cin >> age;
+    cout << "Welcome " << name << endl;
+    cout << age << " years old.";
+}
